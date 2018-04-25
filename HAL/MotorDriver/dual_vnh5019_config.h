ce44b2003ce38e96e1ff6ad2f4d964e3436b7e60
FaridIz <farid_izar@outlook.com>
2018-04-25 00:06:30 -0500

    Eliminate warning by proper declaration of structures in dual_vnh5019.h

--- HAL/MotorDriver/dual_vnh5019_config.h
+++ HAL/MotorDriver/dual_vnh5019_config.h
@@ -10,63 +10,63 @@
 
 #include "PORT_config.h"
 
-PORT_config_t dummy {	// Green LED, just for testing
+PORT_config_t dummy = {	// Green LED, just for testing
 	.port	= ePortE,
 	.pin	= 22,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M1_EN {
+PORT_config_t M1_EN = {
 	.port	= ePortD,
 	.pin	= 15,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M1_INA {
+PORT_config_t M1_INA = {
 	.port	= ePortC,
 	.pin	= 11,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M1_INB {
+PORT_config_t M1_INB = {
 	.port	= ePortD,
 	.pin	= 17,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M1_PWM {
+PORT_config_t M1_PWM = {
 	.port	= ePortA,
 	.pin	= 30,
 	.mux	= eMux2,	// FTM5_CH5
 	.dir	= eOutput,
 };
 
-PORT_config_t M2_EN {
+PORT_config_t M2_EN = {
 	.port	= ePortB,
 	.pin	= 3,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M2_INA {
+PORT_config_t M2_INA = {
 	.port	= ePortD,
 	.pin	= 14,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M2_INB {
+PORT_config_t M2_INB = {
 	.port	= ePortA,
 	.pin	= 31,
 	.mux	= eMuxAsGPIO,
 	.dir	= eOutput,
 };
 
-PORT_config_t M2_PWM {
+PORT_config_t M2_PWM = {
 	.port	= ePortB,
 	.pin	= 0,
 	.mux	= eMux6,	// FTM4_CH6
