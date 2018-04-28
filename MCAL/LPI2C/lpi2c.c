fdf4be242a8733d970b9f442bb6d0bebb255ad0c
Andres Gonzalez Paul <andres.g.paul@gmail.com>
2018-04-28 16:43:28 -0500

    Changes in main.cpp to check compilation with i2c libs

--- MCAL/LPI2C/lpi2c.c
+++ MCAL/LPI2C/lpi2c.c
@@ -9,6 +9,36 @@
 #include "dmamux.h"
 #include "edma.h"
 
+//#if (defined(MCU_S32K142) || defined(MCU_S32K144) || defined(MCU_S32K146) || defined(MCU_S32K148))
+//
+//void LPI2C_init(uint8_t pcc_index, LPI2C_Type* base){
+//    PCC->PCCn[pcc_index ] |= PCC_PCCn_PCS(1)        /* Clk src: System Oscillator*/
+//                              | PCC_PCCn_CGC_MASK;   /* Enable clock for LPI2C0 */
+//
+//    base->MCFGR1 = LPI2C_MCFGR1_PRESCALE(0);	/*Prescaler selection*/
+//
+//    base->MCCR0 = LPI2C_MCCR0_CLKLO(0x03)		/*Clock period low*/
+//                | LPI2C_MCCR0_CLKHI(0x04)		/*Clock period high*/
+//                | LPI2C_MCCR0_SETHOLD(0x03)		/*Setup hold delay*/
+//                | LPI2C_MCCR0_DATAVD(0x01);		/*Data valid delay*/
+//
+//    base->MFCR = LPI2C_MFCR_TXWATER(0)
+//                  |LPI2C_MFCR_RXWATER(3);
+//    base->MCR |= LPI2C_MCR_MEN_MASK /* Enbale LPI2C as master */
+//            | LPI2C_MCR_DBGEN_MASK;
+//}
+//
+//void LPI2C_Transmit (LPI2C_Type* base){
+//    base->MTDR = (0x05<<8)|((0x1E<<1)|0);
+//}
+//
+//void wait(volatile int cycles){
+//    /* Delay function - do nothing for a number of cycles */
+//    while(cycles--);
+//}
+//
+//#endif
+
 /*******************************************************************************
 * Local variables
 *******************************************************************************/
@@ -254,34 +284,4 @@ I2C_ReturnType LPI2C1_Receive(uint8_t *RxBuffer)
 
     return(ret);
 }
-
-//#if (defined(MCU_S32K142) || defined(MCU_S32K144) || defined(MCU_S32K146) || defined(MCU_S32K148))
-//
-//void LPI2C_init(uint8_t pcc_index, LPI2C_Type* base){
-//    PCC->PCCn[pcc_index ] |= PCC_PCCn_PCS(1)        /* Clk src: System Oscillator*/
-//                              | PCC_PCCn_CGC_MASK;   /* Enable clock for LPI2C0 */
-//
-//    base->MCFGR1 = LPI2C_MCFGR1_PRESCALE(0);	/*Prescaler selection*/
-//
-//    base->MCCR0 = LPI2C_MCCR0_CLKLO(0x03)		/*Clock period low*/
-//                | LPI2C_MCCR0_CLKHI(0x04)		/*Clock period high*/
-//                | LPI2C_MCCR0_SETHOLD(0x03)		/*Setup hold delay*/
-//                | LPI2C_MCCR0_DATAVD(0x01);		/*Data valid delay*/
-//
-//    base->MFCR = LPI2C_MFCR_TXWATER(0)
-//                  |LPI2C_MFCR_RXWATER(3);
-//    base->MCR |= LPI2C_MCR_MEN_MASK /* Enbale LPI2C as master */
-//            | LPI2C_MCR_DBGEN_MASK;
-//}
-//
-//void LPI2C_Transmit (LPI2C_Type* base){
-//    base->MTDR = (0x05<<8)|((0x1E<<1)|0);
-//}
-//
-//void wait(volatile int cycles){
-//    /* Delay function - do nothing for a number of cycles */
-//    while(cycles--);
-//}
-//
-//#endif
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,61 @@ extern "C" {
 #include "lpi2c.h"
 }
 
+///* Extra declarations of ports to be used, not already under implementation*/
+//void Port_init_config(void)
+//{
+//	/* Enable clock for PORTs */
+//	PCC->PCCn[PCC_PORTA_INDEX] = PCC_PCCn_CGC_MASK;
+//	PCC->PCCn[PCC_PORTB_INDEX] = PCC_PCCn_CGC_MASK;
+//	PCC->PCCn[PCC_PORTC_INDEX] = PCC_PCCn_CGC_MASK;
+//	PCC->PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK;
+//	PCC->PCCn[PCC_PORTE_INDEX] = PCC_PCCn_CGC_MASK;
+//
+//	/* Configure GPIO outputs for board LEDs */
+//	PORTE->PCR[21] = PORT_PCR_MUX(1);	// Red
+//	PORTE->PCR[22] = PORT_PCR_MUX(1);	// Green
+//	PORTE->PCR[23] = PORT_PCR_MUX(1);	// Blue
+//	PTE->PDDR |= (0b111<<21);
+//
+//	/* Configure SW3 AND SW4 as input buttons */
+//	PORTC->PCR[12] = PORT_PCR_MUX(1)		// Port C12: MUX = GPIO
+//				   | PORT_PCR_PFE(1);		// 			 Input filter enabled
+//	PORTC->PCR[13] = PORT_PCR_MUX(1)		// Port C13: MUX = GPIO
+//				   | PORT_PCR_PFE(1);		// 			 Input filter enabled
+//	PTC->PDDR &= ~(0b11<<12);				// Data direction = input
+//
+//	/* Cruise control driver */
+//	PORTC->PCR[29] = PORT_PCR_MUX(2);		// Port C29: FTM5_CH2		ENA
+//	PORTC->PCR[30] = PORT_PCR_MUX(1);		// Port C30: GPIO-output	IN1
+//	PORTC->PCR[31] = PORT_PCR_MUX(1);		// Port C31: GPIO-output	IN2
+//											// Port ###: GPIO-output	ENB/IN3
+//	PTC->PDDR |= (0b111<<29);
+//}
+//
+///* Temporary ADC function for testing */
+//uint32_t temporary_adc_func(void){
+//	convertAdcChan(0b101100);		/* Convert Channel AD28 to pot on EVB */
+//	while(adc_complete()==0){}      /* Wait for conversion complete flag */
+//	return read_adc_chx();			/* Get channel's conversion results in mv */
+//}
 
-/* Extra declarations of ports to be used, not already under implementation*/
-void Port_init_config(void)
+void PORT_setup(void)
 {
-	/* Enable clock for PORTs */
-	PCC->PCCn[PCC_PORTA_INDEX] = PCC_PCCn_CGC_MASK;
-	PCC->PCCn[PCC_PORTB_INDEX] = PCC_PCCn_CGC_MASK;
-	PCC->PCCn[PCC_PORTC_INDEX] = PCC_PCCn_CGC_MASK;
-	PCC->PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK;
-	PCC->PCCn[PCC_PORTE_INDEX] = PCC_PCCn_CGC_MASK;
+	PCC->PCCn[PCC_PORTD_INDEX] |= PCC_PCCn_CGC_MASK;	/* CGC=1: Clock enabled for PORTD */
+	PCC->PCCn[PCC_PORTC_INDEX] |= PCC_PCCn_CGC_MASK;	/* CGC=1: Clock enabled for PORTC */
 
-	/* Configure GPIO outputs for board LEDs */
-	PORTE->PCR[21] = PORT_PCR_MUX(1);	// Red
-	PORTE->PCR[22] = PORT_PCR_MUX(1);	// Green
-	PORTE->PCR[23] = PORT_PCR_MUX(1);	// Blue
-	PTE->PDDR |= (0b111<<21);
-
-	/* Configure SW3 AND SW4 as input buttons */
-	PORTC->PCR[12] = PORT_PCR_MUX(1)		// Port C12: MUX = GPIO
-				   | PORT_PCR_PFE(1);		// 			 Input filter enabled
-	PORTC->PCR[13] = PORT_PCR_MUX(1)		// Port C13: MUX = GPIO
-				   | PORT_PCR_PFE(1);		// 			 Input filter enabled
-	PTC->PDDR &= ~(0b11<<12);				// Data direction = input
-
-	/* Cruise control driver */
-	PORTC->PCR[29] = PORT_PCR_MUX(2);		// Port C29: FTM5_CH2		ENA
-	PORTC->PCR[30] = PORT_PCR_MUX(1);		// Port C30: GPIO-output	IN1
-	PORTC->PCR[31] = PORT_PCR_MUX(1);		// Port C31: GPIO-output	IN2
-											// Port ###: GPIO-output	ENB/IN3
-	PTC->PDDR |= (0b111<<29);
+	PORTC->PCR[31] |= PORT_PCR_MUX(4)	/* LPI2C1_SDA */
+                   |  PORT_PCR_PE_MASK
+                   |  PORT_PCR_PS(1);
+	PORTD->PCR[19] |= PORT_PCR_MUX(4)	/* LPI2C1_SCL */
+                   |  PORT_PCR_PE_MASK
+                   |  PORT_PCR_PS(1);
 }
-
-/* Temporary ADC function for testing */
-uint32_t temporary_adc_func(void){
-	convertAdcChan(0b101100);		/* Convert Channel AD28 to pot on EVB */
-	while(adc_complete()==0){}      /* Wait for conversion complete flag */
-	return read_adc_chx();			/* Get channel's conversion results in mv */
-}
-
 
 int main()
 {
+	uint8_t send[] = {0x10, 2, 100};
+
 	/* Clocks configuration and initialization */
 	SOSC_init_8MHz();       /* Initialize system oscilator for 8 MHz xtal */
 	SPLL_init_160MHz();     /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
@@ -58,22 +71,25 @@ int main()
 
 	/* Testing section (Provisional stuff) ========================================================= */
 
+	PORT_setup();
+
+	LPI2C1_Init();
+	LPI2C1_WriteBuffer(I2C_WRITE, send, sizeof(send));
+	LPI2C1_Transmit();
+
 //	Port_init_config();
 //	ADC_init();            /* Init ADC resolution 12 bit*/
 //	uint32_t adcval;
-
-	double pos = 0;
-	Steering_init();
-	PWM_set_duty(M1_PWM, 0);
-	GPIO_setPin(M1_INA);
-	GPIO_setPin(M1_EN);
+//	double pos = 0;
+//	Steering_init();
+//	PWM_set_duty(M1_PWM, 0);
+//	GPIO_setPin(M1_INA);
+//	GPIO_setPin(M1_EN);
 
 	for(;;){
-		pos = steering_encoder_read();
+//		pos = steering_encoder_read();
 //		adcval = temporary_adc_func();
-
 	}
-
 
 	/* End of Testing section (Provisional stuff) ================================================== */
 
